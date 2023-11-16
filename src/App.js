c98e359a581d96d466d0695f412c0135d2b488ea
AhmedGemyA <gemya430@gmail.com>
2023-11-16 02:06:52 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react'
+import Weather from './components/weather app/Weather';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div>
+      <Weather />
+    </div>
+  )
+}
+
+export default App
